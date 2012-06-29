2b7761cb0a8a68bdd1905d29c8fceba3a4224b1f
Joshua Napoli <jnapoli@swipely.com>
2012-06-29 17:38:55 -0400

    spec the current user behavior

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -3,9 +3,19 @@ require 'spec_helper'
 describe ContactsController do
 
   describe "GET 'index'" do
+    before { get 'index' }
+
     it "returns http success" do
-      get 'index'
       response.should be_success
+    end
+
+    context 'with a current user' do
+      let(:contact) { Contact.create! email: 'john@example.com', first_name: 'john', last_name: 'doe', phone: '123-567-1234' }
+      before { session[:contact_id] = contact.id }
+
+      it 'should have a current user' do
+        assigns(:current_user).should == contact
+      end
     end
   end
 
